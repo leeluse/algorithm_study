aa01009b5a3797445630a3d0431846f1dc0b9c09
leeluse <122202949+leeluse@users.noreply.github.com>
2023-07-11 09:56:24 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,12 @@
+
+name,age,weight='None',0,0
+while True:
+    name,age,weight=input().split()
+    age=int(age)
+    weight=int(weight)
+    if name=='#' and age==0 and weight==0:
+        break
+    if age>17 or weight>=80:
+        print(name+' '+'Senior')
+    else:
+        print(name+' '+'Junior')
