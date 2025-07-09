eddfee851957bb6ed18f564f76a5ea51e2fa6576
Suyeon Lee <122202949+leeluse@users.noreply.github.com>
2025-07-09 23:47:12 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 40640 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,25 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+const result = [];
+
+function back(arr) {
+  if (arr.length === m) {
+    result.push(arr);
+    return;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!arr.includes(i)) {
+      back([...arr, i]);
+    }
+  }
+}
+
+back([]);
+console.log(result.map((arr) => arr.join(" ")).join("\n"));
+
